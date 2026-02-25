d6854d2da4c2cd01583f6cc467340f7c9d8cee3c
Tenium <amin.behravan@yahoo.com>
2026-02-25 12:17:55 +0330

    Update main.go

--- main.go
+++ main.go
@@ -11,17 +11,40 @@ import (
 	"time"
 )
 
+type WorkerSource struct {
+	Name     string
+	URL      string
+	IsLegacy bool
+}
+
 var (
-	srcPath           string
-	workerPath        string
-	cachePath         string
-	isAndroid         = false
-	VERSION           = "dev"
-	defaultWorkerURLs = []string{
-		"https://raw.githubusercontent.com/10ium/free-config/main/worker/iptv_player.txt",
-		"https://raw.githubusercontent.com/10ium/free-config/main/worker/ClashFa_Mirror_Pro.txt",
-		"https://raw.githubusercontent.com/10ium/free-config/main/worker/great_mihomo_converter_v2%2Bui.txt",
-		"https://raw.githubusercontent.com/10ium/free-config/main/worker/iran_proxy.txt",
+	srcPath       string
+	workerPath    string
+	cachePath     string
+	isAndroid     = false
+	VERSION       = "dev"
+	workerSources = []WorkerSource{
+		{
+			Name:     "Original BPB Worker (legacy mode)",
+			URL:      "https://github.com/bia-pain-bache/BPB-Worker-Panel/releases/latest/download/worker.js",
+			IsLegacy: true,
+		},
+		{
+			Name: "iptv_player",
+			URL:  "https://raw.githubusercontent.com/10ium/free-config/main/worker/iptv_player.txt",
+		},
+		{
+			Name: "ClashFa_Mirror_Pro",
+			URL:  "https://raw.githubusercontent.com/10ium/free-config/main/worker/ClashFa_Mirror_Pro.txt",
+		},
+		{
+			Name: "great_mihomo_converter_v2+ui",
+			URL:  "https://raw.githubusercontent.com/10ium/free-config/main/worker/great_mihomo_converter_v2%2Bui.txt",
+		},
+		{
+			Name: "iran_proxy",
+			URL:  "https://raw.githubusercontent.com/10ium/free-config/main/worker/iran_proxy.txt",
+		},
 	}
 )
 
